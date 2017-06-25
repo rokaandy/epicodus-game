914c06c5b0253020c97f6b0f419aecb6436b0c39
Andy Luc <rokaandy@gmail.com>
2017-06-25 16:44:02 -0700

    Add array and loop methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
+var bounce = [];
+
+
 var ping = function(number) {
-    if (number % 15 === 0) {
+  if (number % 15 === 0) {
     return 'pingpong';
   } else if (number % 3 === 0) {
     return 'ping';
   } else if (number % 5 === 0) {
     return 'pong';
   } else {
-    return false;
+    return number;
   }
 };
 
@@ -14,14 +17,23 @@ var ping = function(number) {
 $(document).ready(function() {
   $("form#games").submit(function(event) {
     event.preventDefault();
+    var outside = [];
     var number = $("input#number").val();
-    var result = ping(number);
-
-    if (result) {
-      $(".ball").text(result);
-    } else {
-      $(".ball").text(number);
+    //var result = ping(number);
+    bounce.push(number);
+    for (var i=0; i<bounce.length; i++) {
+      var nub = ping(bounce[i]);
+      outside.push(nub);
     }
+    $(".ball").text('');
+    for (var i=0; i<outside.length; i++) {
+      $(".ball").append('<p>' + outside[i]) + '</p>';
+    }
+    // if (result) {
+    //   $(".ball").text(result);
+    // } else {
+    //   $(".ball").text(number);
+    // }
 
     $("#result").show();
   });
