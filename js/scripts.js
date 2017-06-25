0a079f7719512200a38467aa69ec3c9735260ae2
Andy Luc <rokaandy@gmail.com>
2017-06-25 12:18:39 -0700

    Modify all files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-$(document).ready(function(); {
-  $("#games form").submit(function(event) {
+var ping = function(number) {
+  if (number % 3 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 
+$(document).ready(function() {
+  $("form#games").submit(function(event) {
     event.preventDefault();
+var number = parseInt($("input#number").val());
+var result = ping(number);
+
+$(".ping").text(number);
+
+    $("#result").show();
   });
 });
